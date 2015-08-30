6d7ad6cdbae5a2ad4468f83ed7f4b4d3c81c00f8
Ian Horswill <ian@northwestern.edu>
2015-08-30 17:56:00 -0500

    Doors now auto open/close

--- Assets/TileCore/Door.cs
+++ Assets/TileCore/Door.cs
@@ -8,6 +8,7 @@ class Door : MonoBehaviour
     public Sprite ClosedSprite;
     public Sprite OpenSprite;
     public bool InitiallyOpen;
+    public bool LeaveClosed;
     public bool Locked;
 #pragma warning restore 0649
 
@@ -28,14 +29,24 @@ class Door : MonoBehaviour
         }
     }
 
-    public void ForceDoorState(bool value)
+    public void ForceDoorState(bool open)
     {
-        this.GetComponent<Collider2D>().enabled = !value;
-        this.GetComponent<SpriteRenderer>().sprite = value ? this.OpenSprite : this.ClosedSprite;
+        this.GetComponent<SpriteRenderer>().sprite = open ? this.OpenSprite : this.ClosedSprite;
+        this.GetComponent<Collider2D>().isTrigger = !Locked;
     }
 
     internal void Awake()
     {
         this.ForceDoorState(InitiallyOpen);
+    }
+
+    internal void OnTriggerEnter2D(Collider2D enterer)
+    {
+        Open = true;
+    }
+
+    internal void OnTriggerExit2D(Collider2D exiter)
+    {
+        Open = !LeaveClosed;
     }
 }
